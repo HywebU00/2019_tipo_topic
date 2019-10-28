e055a2571f77063e65fa9ce44a72380d6577ef9b
Veela <veela.tseng@hyweb.com.tw>
2019-10-28 17:10:05 +0800

    first commit

--- js/customize.js
+++ js/customize.js
@@ -3,7 +3,7 @@ $(function() {
     // 首頁輪播
     $('.mpSlider').slick({
         mobileFirst: true,
-        dots: true,
+        dots: false,
         arrow: true,
         infinite: true,
         speed: 500,
@@ -13,15 +13,13 @@ $(function() {
         lazyLoad: 'ondemand',
         ease: 'ease'
     });
-    // 廣告輪播
-    $('.adSlider').slick({
-        mobileFirst: true,
+    $('.mp_block ul.link').slick({
         dots: false,
         infinite: true,
         speed: 300,
-        slidesToShow: 2,
+        slidesToShow: 4,
         slidesToScroll: 1,
-        autoplay: true,
+        autoplay: false,
         arrow: true,
         lazyLoaded: true,
         lazyLoad: 'ondemand',
@@ -29,25 +27,31 @@ $(function() {
         responsive: [{
             breakpoint: 1200,
             settings: {
-                slidesToShow: 5,
-                slidesToScroll: 1,
-                arrows: true
+                slidesToShow: 3
             }
         },{
             breakpoint: 768,
             settings: {
-                slidesToShow: 4,
-                slidesToScroll: 1,
-                arrows: true
+                slidesToShow: 3
             }
         },{
             breakpoint: 575,
             settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-                arrows: true
+                slidesToShow: 2
             }
         }]
+    });
+    $('.video ul').slick({
+        mobileFirst: true,
+        dots: true,
+        arrow: true,
+        infinite: true,
+        speed: 500,
+        autoplay: false,
+        fade: true,
+        lazyLoaded: true,
+        lazyLoad: 'ondemand',
+        ease: 'ease'
     });
     //燈箱slick+lightBox組合
     $('.cp_slider').slick({
@@ -97,11 +101,11 @@ $(function() {
         lazy: true
     });
      // cp_photo
-    $('.Slider-for').on('init reInit afterChange', function(event, slick, currentSlide) {
+     $('.Slider-for').on('init reInit afterChange', function(event, slick, currentSlide) {
         var i = (currentSlide ? currentSlide : 0) + 1;
         $('.controls').html(i + '/' + slick.slideCount);
     });
-    $('.Slider-for').slick({
+     $('.Slider-for').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         arrows: false,
@@ -112,7 +116,7 @@ $(function() {
         asNavFor: '.Slider-nav',
         infinite: true
     });
-    $('.Slider-nav').slick({
+     $('.Slider-nav').slick({
         slidesToShow: 2,
         slidesToScroll: 1,
         asNavFor: '.Slider-for',
@@ -122,4 +126,4 @@ $(function() {
         focusOnSelect: true,
         infinite: true
     });
-});
+ });
